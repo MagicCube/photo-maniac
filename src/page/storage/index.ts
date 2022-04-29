32d511e5b21681e11f49052161d8ab3f22630370
Henry Li <henry1943@163.com>
2022-04-29 17:19:47 +0800

    feat: error handling

--- src/page/storage/index.ts
+++ src/page/storage/index.ts
@@ -3,9 +3,11 @@ import type { Photo } from '@/types';
 import { localPhotos } from './local-photos';
 
 export async function nextPhoto(): Promise<Photo | null> {
-  const store = await chrome.storage.local.get(['photo.next']);
-  if (store && store['photo.next']) {
-    return store['photo.next'];
-  }
+  try {
+    const store = await chrome.storage.local.get(['photo.next']);
+    if (store && store['photo.next']) {
+      return store['photo.next'];
+    }
+  } catch {}
   return localPhotos[Math.floor(Math.random() * localPhotos.length)];
 }
