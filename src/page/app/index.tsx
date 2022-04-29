ddf2d5ad03049f55d90388b6205b53dea411ecfb
Henry Li <henry1943@163.com>
2022-04-29 17:26:00 +0800

    refactor: extract prop 'photo'

--- src/page/app/index.tsx
+++ src/page/app/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import type { Photo } from '@/types';
+import type { Message } from '@/messaging';
+
+import { nextPhoto } from '../storage';
 import { PhotoView } from '../components/PhotoView';
 
 export function App() {
+  const [photo, setPhoto] = useState<Photo | null>(null);
+  useEffect(() => {
+    nextPhoto().then((photo) => {
+      setPhoto(photo);
+      chrome.runtime.sendMessage({
+        type: 'photomaniac.commands.nextPhoto',
+      } as Message);
+    });
+  }, []);
   return (
     <div className="app">
-      <PhotoView />
+      <PhotoView photo={photo} />
     </div>
   );
 }
--- src/page/components/PhotoView/index.tsx
+++ src/page/components/PhotoView/index.tsx
@@ -1,26 +1,20 @@
-import { useEffect, useState } from 'react';
-
-import type { Message } from '@/messaging';
 import type { Photo } from '@/types';
 
-import { nextPhoto } from '../../storage';
+export interface PhotoViewProps {
+  photo: Photo | null;
+}
 
-export function PhotoView() {
-  const [photo, setPhoto] = useState<Photo | null>(null);
-  useEffect(() => {
-    nextPhoto().then((photo) => {
-      setPhoto(photo);
-      chrome.runtime.sendMessage({
-        type: 'photomaniac.commands.nextPhoto',
-      } as Message);
-    });
-  }, []);
-  return (
-    <div
-      className="photoView"
-      style={{
-        backgroundImage: `url(${photo?.images[0].webpUrl})`,
-      }}
-    ></div>
-  );
+export function PhotoView({ photo }: PhotoViewProps) {
+  if (photo) {
+    return (
+      <div
+        className="photoView"
+        style={{
+          backgroundImage: `url(${photo?.images[0].webpUrl})`,
+        }}
+      ></div>
+    );
+  } else {
+    return null;
+  }
 }
