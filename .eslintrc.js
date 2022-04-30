e4a6801dea12395ff7b1fa121cb4a9afed0ebdb1
Henry Li <henry1943@163.com>
2022-04-30 13:24:38 +0800

    chore: add eslint for React

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,12 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   rules: {
     '@typescript-eslint/consistent-type-imports': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 };
