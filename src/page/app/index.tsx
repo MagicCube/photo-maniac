117dbab8e7e5c67f2201d267083b32dacff2440a
Henry Li <henry1943@163.com>
2022-05-02 09:32:35 +0800

    refactor: extract change Photo

--- src/page/app/index.tsx
+++ src/page/app/index.tsx
@@ -15,15 +15,20 @@ export function App() {
   const [photo, setPhoto] = useState<Photo | null>(null);
   const [fallback, setFallback] = useState(false);
   const [isUpdating, setUpdating] = useState(false);
+  const changePhoto = useCallback((photo: Photo) => {
+    setPhoto(photo);
+    document.title = `New Tab${photo?.name && ` - ${photo.name}`}`;
+    console.info(`Photo${photo?.name && ` - ${photo.name}`}`, photo);
+  }, []);
   const update = useCallback(() => {
     StorageService.update().then(() => {
       const photo = StorageService.data.nextPhoto;
-      console.info(photo);
-      document.title = `New Tab${photo?.name && ` - ${photo.name}`}`;
-      setPhoto(photo);
-      MessageService.publish('photomaniac.commands.nextPhoto');
+      if (photo) {
+        changePhoto(photo);
+        MessageService.publish('photomaniac.commands.nextPhoto');
+      }
     });
-  }, []);
+  }, [changePhoto]);
   const fallbackToNextPhoto = useCallback(() => {
     if (fallback) {
       return;
@@ -31,9 +36,8 @@ export function App() {
     setFallback(true);
     const fallbackPhoto =
       FALLBACK_PHOTOS[Math.floor(Math.random() * FALLBACK_PHOTOS.length)];
-    setPhoto(fallbackPhoto);
-    console.info(fallbackPhoto);
-  }, [fallback]);
+    changePhoto(fallbackPhoto);
+  }, [changePhoto, fallback]);
   useEffect(() => {
     MessageService.subscribe('photomaniac.events.photosUpdated', () => {
       update();
@@ -55,10 +59,12 @@ export function App() {
     setCategories(values);
     StorageService.saveCategories(values);
   }, []);
-  const handlePhotoSelect = useCallback((photo: Photo) => {
-    setPhoto(photo);
-    document.title = `New Tab${photo?.name && ` - ${photo.name}`}`;
-  }, []);
+  const handlePhotoSelect = useCallback(
+    (photo: Photo) => {
+      changePhoto(photo);
+    },
+    [changePhoto]
+  );
   const handleUpdate = useCallback(() => {
     setUpdating(true);
     MessageService.publish('photomaniac.commands.updatePhotos');
