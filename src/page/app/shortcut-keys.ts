a0a8002834a452650d54f6115508e8f8f285b971
Henry Li <henry1943@163.com>
2022-05-05 14:27:13 +0800

    feat: support fullscreen mode

--- src/page/app/shortcut-keys.ts
+++ src/page/app/shortcut-keys.ts
@@ -1,7 +1,8 @@
 import { useCallback, useEffect } from 'react';
 
-import type { Photo } from '@/types';
 import { StorageService } from '@/storage';
+import type { Photo } from '@/types';
+
 import { showMessage } from './Message';
 
 export function useShortcutKeys({ photo }: { photo: Photo | null }) {
@@ -16,11 +17,22 @@ export function useShortcutKeys({ photo }: { photo: Photo | null }) {
         ) {
           switch (event.key.toLowerCase()) {
             case 'b':
+              // Add to blacklist
               event.preventDefault();
               StorageService.addToBlacklist(photo);
               showMessage('Added to blacklist');
               break;
+            case 'f':
+              // Toggle fullscreen
+              event.preventDefault();
+              if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+              } else {
+                document.exitFullscreen();
+              }
+              break;
             case 'v':
+              // Visit 500px.com to see the detail of the photo.
               event.preventDefault();
               window.open(`https://500px.com/photo/${photo.legacyId}/`);
               break;
--- src/page/components/PhotoView/index.tsx
+++ src/page/components/PhotoView/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from 'react';
+
 import type { Photo } from '@/types';
 
 export interface PhotoViewProps {
@@ -6,6 +8,20 @@ export interface PhotoViewProps {
 }
 
 export function PhotoView({ data, onError }: PhotoViewProps) {
+  const [fullscreen, setFullscreen] = useState(false);
+  const handleFullscreenChange = useCallback(() => {
+    setFullscreen(document.fullscreenElement !== null);
+  }, []);
+  useEffect(() => {
+    document.addEventListener('fullscreenchange', handleFullscreenChange, true);
+    return () => {
+      document.removeEventListener(
+        'fullscreenchange',
+        handleFullscreenChange,
+        true
+      );
+    };
+  }, [handleFullscreenChange]);
   if (data) {
     let url = data?.images[0].webpUrl;
     if (url === 'local') {
@@ -14,6 +30,7 @@ export function PhotoView({ data, onError }: PhotoViewProps) {
     return (
       <img
         className="pm-photo-view"
+        style={{ objectFit: fullscreen ? 'contain' : undefined }}
         src={url}
         onError={() => {
           onError();
